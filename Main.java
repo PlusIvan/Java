2a4dcf69a323bd8c219ce8fcecb4d1f575a2147d
PlusIvan <ivan@ivanmoroz.net>
2019-12-21 01:36:39 +0000

    upload

---
+++ Main.java
@@ -0,0 +1,14 @@
+/*
+!!! Important
+This project was coded in VSC (Visual Studio Code). Changes may appeal behind IDE.
+-- Ivan Moroz
+*/
+
+public class Main {
+    public static void main(String[] args) {
+        Test ivan = new Test("Moroz");
+        System.out.println(ivan.getName());
+
+        
+    }
+}
---
+++ Test.java
@@ -0,0 +1,9 @@
+public class Test {
+    private String name;
+    public Test(String name) {
+        this.name = name;
+    }
+    public String getName(){
+        return this.name;
+    }
+}
