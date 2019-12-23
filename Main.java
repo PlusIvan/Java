1f0e242d57bddd5f8d3ac5572a210ba72c6681ed
PlusIvan <ivan@ivanmoroz.net>
2019-12-23 18:24:05 +0000

    update

--- Main.java
+++ Main.java
@@ -1,13 +1,11 @@
 /*
-!!! Important
-This project was coded in VSC (Visual Studio Code). Changes may appeal behind IDE.
--- Ivan Moroz
+
 */
 
 public class Main {
     public static void main(String[] args) {
-        Test ivan = new Test("Moroz");
-        System.out.println(ivan.getName());
+       // Test ivan = new Test("Moroz");
+       // System.out.println(ivan.getName());
 
         
     }
--- README.md
+++ README.md
@@ -1,6 +1,2 @@
 # Java
-just for learning
-
-Anything you see here...is nothing important but just for me to learn better java...
-
-I am not responsable if i ended up writing a trojan horse and you debug it
+Just random stuff, learning java myself.
