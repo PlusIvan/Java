ff2407a8f0bc64c885294ac49987b7e1076267ac
PlusIvan <ivan@ivanmoroz.net>
2020-01-07 14:55:22 +0000

    Update Recursion.java

--- Tests/Recursion.java
+++ Tests/Recursion.java
@@ -1,13 +1,27 @@
+import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.Scanner;
 
 public class Recursion {
 
-
     public static void main(String[] args) {
+        Scanner myObj = new Scanner(System.in);  
 
-        LinkedList list = new LinkedList<String>();
-        list.add("Ivan");
-        System.out.println(list);
+      ArrayList names = new ArrayList<String>();
+
+
+        while(names.size() != 5){
+            System.out.println("Insert a name? "+names.size()+"/5\n");
+            String name = myObj.nextLine();
+            names.add(name);
+        }
+
+        for (int i = 0; i < names.size(); i++) {
+            System.out.println(names.get(names.size()-i));
+        }
+       
+
+
 
     }
 
